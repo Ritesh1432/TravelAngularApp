917eb88218dbff88315dfbfca66b687d9827600d
Ritesh1432 <riteshdewoolkar@gmail.com>
2021-06-27 11:02:44 +0530

    Conn to MongoDb Atlas

--- server/models/db.js
+++ server/models/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require('./user_model');
 require('./flight_model');
 
-const conn_str = "mongodb://localhost:27017/TravelSystem";
+const conn_str = 'mongodb+srv://RItesh:rQRewIhciZnpFRjX@cluster0.igu49.mongodb.net/TravelSystem?retryWrites=true&w=majority';
 mongoose.connect(conn_str, { useUnifiedTopology: true, useNewUrlParser: true, useUnifiedTopology: true })
 	.then( () => console.log("Connected Successfully..."))
     .catch( (error) => console.log(error));
